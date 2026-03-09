58c59c818667451fbf7ef861eb2fb0062e1ccc50
happybanana78 <leonardo.mazzoleni1@gmail.com>
2026-03-09 15:09:32 +0100

    Refactor `auth-service` to encapsulate app logic in `Application` struct and add test scaffolding

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,34 @@
+use axum::serve::Serve;
+use axum::Router;
+use std::error::Error;
+use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
+
+// This struct encapsulates our application-related logic.
+pub struct Application {
+    server: Serve<TcpListener, Router, Router>,
+    // address is exposed as a public field
+    // so we have access to it in tests.
+    pub address: String,
+}
+
+impl Application {
+    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+        let assets_dir = ServeDir::new("assets");
+        let router = Router::new().fallback_service(assets_dir);
+
+        let listener = TcpListener::bind(address).await?;
+
+        let server = axum::serve(listener, router);
+
+        Ok(Self {
+            server,
+            address: address.to_string(),
+        })
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        println!("listening on {}", &self.address);
+        self.server.await
+    }
+}
